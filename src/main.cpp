a0dc753b77bc689622c6e43aaa0c9a418de90977
Gabriel Nogueira <30876329+Gabriel-gn@users.noreply.github.com>
2019-10-15 22:54:26 -0300

    toggle funcionando. ajustes gerais.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,25 @@
 #include <IoAbstraction.h>
 
 const int ledPin = LED_BUILTIN;
-int ledOn = HIGH;
+int ledState = HIGH;
 IoAbstractionRef ioDevice = ioUsingArduino();
 
-
 void toggle() {
-	ledOn = !ledOn; // toggle the LED state.
+    ledState = !ledState;  // toggle the LED state.
+	ioDeviceDigitalWriteS(ioDevice, ledPin, ledState);
+	//Serial.println(ledOn);
 }
 
 void setup() {
-	ioDevicePinMode(ioDevice, ledPin, OUTPUT);
-  ioDeviceDigitalWriteS(ioDevice, ledPin, ledOn);
-	taskManager.scheduleFixedRate(1000, toggle);
+	Serial.begin(115200);
+
+    ioDevicePinMode(ioDevice, ledPin, OUTPUT); // define como output o LED_BUILTIN
+    ioDeviceDigitalWriteS(ioDevice, ledPin, ledState); // define o ledPin como ligado. Lógica inversa no esp8266. 
+
+    taskManager.scheduleFixedRate(1000, toggle); //chama o toggle a cada 1 segundo
 }
 
-
 void loop() {
-	// this is all we should do in loop when using task manager.
-	taskManager.runLoop();
+    // this is all we should do in loop when using task manager.
+    taskManager.runLoop();
 }
